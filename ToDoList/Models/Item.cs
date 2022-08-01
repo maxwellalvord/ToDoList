907f9f512d319df20d76278c202ea1eae73cad60
Maxwell Alvord <maxwellalvord@gmail.com>
2022-08-01 11:51:30 -0700

    WIP add checkbox connected to mysql
    Co-authored-by: matt wilkinson <wilkinsonmatt01@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -7,10 +7,13 @@ namespace ToDoList.Models
         public Item()
         {
             this.JoinEntities = new HashSet<CategoryItem>();
+            Completed = false;
         }
 
         public int ItemId { get; set; }
         public string Description { get; set; }
+        
+        public bool Completed { get; set; } 
 
         public virtual ICollection<CategoryItem> JoinEntities { get;}
     }
